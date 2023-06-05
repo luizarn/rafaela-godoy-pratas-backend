a7dceee354639ee068cc5d3bba7ece9bd3afe83b
luizarn <luizadriven@gmail.com>
2023-06-05 20:39:09 -0300

    feat: implement authentication

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import { connectDb, disconnectDB, loadEnv } from './config';
+import { usersRouter } from './routers/users-router';
+import { authenticationRouter } from './routers/authentication-router';
 
 // import { loadEnv, connectDb, disconnectDB } from '@/config';
 
@@ -11,15 +13,16 @@ app
   .use(cors())
   .use(express.json())
   .get('/health', (_req, res) => res.send('OK!'))
+  .use('/users', usersRouter)
+  .use('/auth', authenticationRouter);
 
 export function init(): Promise<Express> {
   connectDb();
   return Promise.resolve(app);
 }
-  
+
 export async function close(): Promise<void> {
   await disconnectDB();
 }
-   
 
 export default app;
