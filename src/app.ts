e92388113bb83fa7f7bfa7a7a1a795fe646bd324
luizarn <luizadriven@gmail.com>
2023-06-04 22:31:26 -0300

    feat: implement initial configs

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+import { connectDb, disconnectDB, loadEnv } from './config';
+
+// import { loadEnv, connectDb, disconnectDB } from '@/config';
+
+loadEnv();
+
+const app = express();
+app
+  .use(cors())
+  .use(express.json())
+  .get('/health', (_req, res) => res.send('OK!'))
+
+export function init(): Promise<Express> {
+  connectDb();
+  return Promise.resolve(app);
+}
+  
+export async function close(): Promise<void> {
+  await disconnectDB();
+}
+   
+
+export default app;
