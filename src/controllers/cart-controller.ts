32fdc3d8a561615342275f71e64254e8738bb93c
luizarn <luizadriven@gmail.com>
2023-06-22 14:33:52 -0300

    fix: create or update storage for not have duplicate items

--- src/controllers/cart-controller.ts
+++ src/controllers/cart-controller.ts
@@ -4,12 +4,12 @@ import { Prisma } from '@prisma/client';
 import { AuthenticatedRequest } from '@/middlewares';
 import cartService from '@/services/cart-service';
 
-export async function createCartItem(req: AuthenticatedRequest, res: Response) {
+export async function createOrUpdateCartItem(req: AuthenticatedRequest, res: Response) {
   try {
     const { userId } = req;
     const { productId, quantity } = req.body;
 
-    const cart = await cartService.createCartItem(userId, productId, quantity);
+    const cart = await cartService.createOrUpdateCartItem(userId, productId, quantity);
     return res.status(httpStatus.CREATED).json({ id: cart.id });
   } catch (error) {
     return res.status(httpStatus.BAD_REQUEST).send(console.log(error));
